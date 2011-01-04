ec4b79df14c26560d9b6c1429e7b3087494d2ef5
Sidney Burks <sid137@gmail.com>
2011-01-04 03:00:49 +0100

    set Haml::Template.options[:ugly] to false to render pretty prodcutiion HTML

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+Haml::Template.options[:ugly] = false
 
 # Initialize the rails application
 Deluxe::Application.initialize!
